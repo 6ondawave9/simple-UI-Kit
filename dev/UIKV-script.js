ceb305f07fe0efd2c80213167ea8693190dc9de2
Alex <99803886+6ondawave9@users.noreply.github.com>
2022-12-14 18:39:14 +0300

    added possibility to specify the width

--- dev/UIKV-script.js
+++ dev/UIKV-script.js
@@ -1,6 +1,6 @@
 class UIKVCustomSelect {
     constructor(args) {
-        const {blockId, options, defaultValue} = args
+        let {blockId, options, defaultValue, width} = args
         let block = document.querySelector(`#${blockId}`)
         this.options = options
         this.val = defaultValue ? defaultValue : options[0]
@@ -10,10 +10,9 @@ class UIKVCustomSelect {
 
         let longestOption = this.options.reduce((max, n) => max.length > n.length ? max : n, '');
         longestOption = longestOption.length >= this.val.length ? longestOption : this.val
-        box.style.width = longestOption.length * 8 + 18 + 'px'
+        width = width ? width : longestOption.length * 8 + 18 + 'px'
+        box.style.width = width
 
-        box.style.display = 'flex'
-        box.style.alignItems = 'center'
         block.appendChild(box)
 
         this.choosedOption = document.createElement('div')
@@ -29,29 +28,15 @@ class UIKVCustomSelect {
         optionslist.style.top = box.getBoundingClientRect().height + 'px'
         block.appendChild(optionslist)
         optionslist.style.display = 'none'
-
-        block.addEventListener('click', ()=>{
-            if (optionslist.style.display != 'none') {
-                arrow.style.transform = 'rotate(-45deg)'
-                optionslist.style.display = 'none'
-                box.style.borderBottomLeftRadius = '10px'
-                box.style.borderBottomRightRadius = '10px'
-                box.style.border = ''
-            } else {
-                arrow.style.transform = 'rotate(135deg)'
-                optionslist.style.display = 'block'
-                box.style.borderBottomLeftRadius = '0px'
-                box.style.borderBottomRightRadius = '0px'
-                box.style.border = '2px solid var(--dark-color)'
-            }
-        })
         
         this.optionsBlocks = []
 
         this.options.forEach(el => {
             let optionBlock = document.createElement('div')
             optionBlock.classList.add('UIKV-custom-select-option')
+
             optionBlock.style.width = Math.ceil(block.getBoundingClientRect().width) + 'px'
+
             optionBlock.innerText = el
             if (el == this.val) {
                 optionBlock.style.textDecoration = 'underline'
@@ -69,11 +54,17 @@ class UIKVCustomSelect {
 
         document.addEventListener('click', (e)=>{
             if (e.target != box && e.target != this.choosedOption && e.target != arrow) {
+                arrow.style.transform = 'rotate(-45deg)'
                 optionslist.style.display = 'none'
                 box.style.borderBottomLeftRadius = '10px'
                 box.style.borderBottomRightRadius = '10px'
                 box.style.border = ''
-                arrow.style.transform = 'rotate(-45deg)'
+            } else {
+                arrow.style.transform = 'rotate(135deg)'
+                optionslist.style.display = 'block'
+                box.style.borderBottomLeftRadius = '0px'
+                box.style.borderBottomRightRadius = '0px'
+                box.style.border = '2px solid var(--dark-color)'
             }
         })
     }
